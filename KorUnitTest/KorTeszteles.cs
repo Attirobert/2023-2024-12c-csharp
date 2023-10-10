d587e07ea4a0d4a9ff4227107fd57bc8a587f7d5
Attirobert <karobatti@gmail.com>
2023-10-10 13:06:33 +0200

    Teszt folytatás

---
+++ KorUnitTest/KorTeszteles.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Kor;
+
+namespace KorUnitTest
+{
+    [TestClass]
+    public class KorTeszteles
+    {
+        [TestMethod]
+        public void TeruletTeszt()
+        {
+            // Arrange - Környezet beállítása
+            // Példányosítom az osztályt
+            KorClass kr = new KorClass(3);
+            kr.setKerulet();
+            kr.setTerulet();
+
+            // Act - Futtatás
+            double vartEredm = 28.26;
+            double kapottEredm = kr.getTerulet();
+
+            // Assert - Kiértékelés
+            Assert.AreEqual(vartEredm, kapottEredm);
+        }
+    }
+}
