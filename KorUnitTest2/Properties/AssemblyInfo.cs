13e903976456a6aaa5a3577c20b340eb454ccf18
Attirobert <karobatti@gmail.com>
2023-10-24 13:05:10 +0200

    Unit teszt folytatva

---
+++ KorUnitTest2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KorUnitTest2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KorUnitTest2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1784610-1d3a-48d3-accb-7a9e168db6ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KorUnitTest2/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Kor;
+namespace KorUnitTest2
+{
+    [TestClass]
+    public class KorClass_Test
+    {
+        private static double sugar = 3.0,
+            vartEredm = 28.27,
+                kapottEredm;
+        KorClass kr = new KorClass(sugar);
+
+        [TestMethod]
+        public void TeruletTest()
+        {
+            //Arrange - Teszteset beállítása
+            kr.setTerulet();
+
+            // Act - Végrehajtás
+            kapottEredm = Math.Round(kr.getTerulet(),2);
+
+            // Assert - Ellenőrzés
+            Assert.AreEqual(kapottEredm, vartEredm);
+        }
+        [TestMethod]
+        public void KeruletTest()
+        {
+            //Arrange - Teszteset beállítása
+            double vartEredm = 18.85;
+
+            kr.setKerulet();
+
+            // Act - Végrehajtás
+            kapottEredm = Math.Round(kr.getKerulet(),2);
+
+            // Assert - Ellenőrzés
+            Assert.AreEqual(kapottEredm, vartEredm);
+        }
+    }
+}
